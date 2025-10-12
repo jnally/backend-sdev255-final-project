93f197f216e2ded35c771cf8caf0727f7cd36bb7
Jeremy Nally <jnally3@ivytech.edu>
2025-10-12 15:59:05 -0400

    added example course in comment - jn

--- models/course.js
+++ models/course.js
@@ -1,10 +1,16 @@
 const db = require("../db");
 
+// name: "Quantitative Reasoning"
+// subject: "MATH"
+// number: 123
+// description: "Introduces students to the mathematics required for informed citizenship, decision making, reasoning from evidence, working with real world data, and effective communication. Students will solve problems using proportional reasoning, percentages, rates of change, linear and exponential models with applications from statistics and finance."
+// credits: 3
+
 const Course = db.model("Course",{
     name: {type:String, require:true},
+    subject: String,
     number: Number,
     description: String,
-    subject: String,
     credits: {type:Number, min:1, max:5}
 });
 
